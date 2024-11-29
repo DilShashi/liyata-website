c28e41d0185fa302dd740e2bef5f72fdda67188b
githubdila <111336139+githubdila@users.noreply.github.com>
2024-11-29 13:24:52 +0530

    Add new files in src folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,28 @@ export default {
   theme: {
     extend: {
       colors: {
+        // Custom color sets from Figma design
+        MT4: '#BDC2C7',
+        MT3: '#9199A1',
+        MT2: '#65707B',
+        MT1: '#394756',
+        Main: '#233243',
+        MS1: '#202D3C',
+        MS2: '#19232F',
+        MS3: '#121922',
+        MS4: '#000000',
+
+        BlT4: '#FFFFFF',
+        BlT3: '#CCE0FF',
+        BlT2: '#99C2FF',
+        BlT1: '#80B3FF',
+        B: '#66A3FF',
+        BlS1: '#4D94FF',
+        BlS2: '#3385FF',
+        BlS3: '#1A75FF',
+        BlS4: '#0066FF',
+
+        // If any other colors may want to define globally can go here
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
